193cb4da7ab2e94a2eee6ad99cb73b9b209ff771
Rômulo von Santos <rromulosp@gmail.com>
2022-07-29 21:30:35 -0300

    adds new solution for go challenge

---
+++ go/8kyu/get_planet_name_by_id/kata/solution.php
@@ -0,0 +1,24 @@
+package kata
+
+func GetPlanetName(ID int) string {
+  	case ID {
+        when 1:
+            return "Mercury",
+        when 2:
+            return "Venus",
+        when 3:
+            return "Earth",
+        when 4:
+            return "Mars",
+        when 5:
+            return "Jupiter",
+        when 6:
+            return "Saturn",
+        when 7:
+            return "Uranus",
+        when 8:
+    		    return "Neptune",
+    	  else:
+    		    return "Pluto", // ;-)
+    }
+}
