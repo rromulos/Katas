8126a089a7a9049a1b984c6cd15746e3a0cf96b7
Rômulo von Santos <rromulosp@gmail.com>
2022-08-08 22:13:03 -0300

    solution for reserved_string in golang

---
+++ go/8kyu/reversed_string/solution.go
@@ -0,0 +1,10 @@
+package kata
+
+func Solution(word string) string {
+	runes := []rune(word)
+
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
