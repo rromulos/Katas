0d706ec6db12ab98d0564dd9470a25e4b61717ae
Rômulo Santos <rromulosp@gmail.com>
2022-12-10 14:19:58 -0300

    Update solution.java

--- java/6kyu/detect_pangram/solution.java
+++ java/6kyu/detect_pangram/solution.java
@@ -2,9 +2,9 @@ import java.util.LinkedHashSet;
 
 public class PangramChecker {
   public boolean check(String sentence){
-	sentence = sentence.replaceAll("\\s+", "");
+    sentence = sentence.replaceAll("\\s+", "");
     sentence = sentence.replaceAll("[^a-zA-Z0-9]", ""); 
-	sentence = sentence.toLowerCase();
+    sentence = sentence.toLowerCase();
     LinkedHashSet<Character> set = new LinkedHashSet<>();   
     
     for(int i = 0; i < sentence.length();i++) {
